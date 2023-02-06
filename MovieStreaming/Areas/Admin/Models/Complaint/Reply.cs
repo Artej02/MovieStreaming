b45de23db68bf055581e98988347d1047894c02e
eldivuniqi <ev52271@ubt-uni.net>
2023-02-06 16:36:32 +0100

    Complaint added

---
+++ MovieStreaming/Areas/Admin/Models/Complaint/Reply.cs
@@ -0,0 +1,20 @@
+﻿using Org.BouncyCastle.Bcpg.OpenPgp;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieStreaming.Areas.Adim.Models.Complaint
+{
+    public class Reply
+    {
+        public int Id { get; set; }
+        public int TicketId { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+        public int UserId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Email { get; set; }
+    }
+}
