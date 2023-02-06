cbf36d8cd04539acc4b82e7680683078b9b7f5b1
AltinSadiku <altinsadiku53@gmail.com>
2023-02-06 04:14:52 -0800

    View Authorization added

---
+++ MovieStreaming/Areas/Admin/Models/ViewAuthorization/ViewAuthorization.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace MovieStreaming.Areas.Admin.Models.ViewAuthorization
+{
+    public class ViewAuthorization
+    {
+        [Key]
+        public int? ID { get; set; }
+        [Required]
+        public int ViewID { get; set; }
+        [Required]
+        public int RoleID { get; set; }
+        [Required]
+        public int AuthorizationTypeID { get; set; }
+        public int? CreatedUserId { get; set; }
+        public int? UpdatedUserId { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public string ViewURL { get; set; }
+    }
+}
